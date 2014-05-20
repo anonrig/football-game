b18f4b2f49bdc0f635da6ac25166cbe0d12e4a8e
ynizipli <ynizipli@sabanciuniv.edu>
2014-05-20 17:44:41 +0300

    Remove unnecessary libraries.

--- Fubio-Android/src/com/st/fubio_android/FB_Login.java
+++ Fubio-Android/src/com/st/fubio_android/FB_Login.java
@@ -7,9 +7,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
