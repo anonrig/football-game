99f3bd88a8a46c4070f54e902fe4eb8f5bc6db2f
ynizipli <ynizipli@sabanciuniv.edu>
2014-05-22 00:20:07 +0300

    Add about activity.

---
+++ Fubio-Android/src/com/st/fubio_android/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.st.fubio_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class AboutActivity extends MainFragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_about);
+
+	}
+
+}
