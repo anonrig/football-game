d182c4050227d9cd33dfe4ccdbce688f985b0271
ynizipli <ynizipli@sabanciuniv.edu>
2014-05-23 17:38:39 +0300

    Fix chooseTeamActivity listview and onItemClickListener.

--- Fubio-Android/src/com/st/fubio_android/ChooseTeamActivity.java
+++ Fubio-Android/src/com/st/fubio_android/ChooseTeamActivity.java
@@ -3,11 +3,14 @@ package com.st.fubio_android;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 import com.st.fubio_android.Models.Team;
 import com.st.fubio_android.ServerConnections.RequestManager;
+import com.st.fubio_android.ServerConnections.TinyDB;
 
 import android.app.Activity;
 import android.app.ActionBar;
@@ -16,11 +19,14 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -92,6 +98,54 @@ public class ChooseTeamActivity extends MainFragment {
 				String[] titles = deneme.toArray(new String[deneme.size()]);
 				
 		        lv.setAdapter(new ArrayAdapter<String>(mf, android.R.layout.simple_list_item_1, titles));
+		        
+		        lv.setOnItemClickListener(new OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view,
+							int position, long id) {
+							Team chosenTeam = teamList.get(position);
+							
+							RequestManager rm = new RequestManager(getApplicationContext());
+							RequestParams req = new RequestParams();
+							
+							req.put("teamId", chosenTeam.getId());
+							
+							rm.post("player/team", req, new AsyncHttpResponseHandler() {
+				            	
+				            	@Override
+								public void onStart() {
+									Log.v("TeamSelect", "onStart");
+								}
+
+				            	
+								@Override
+								public void onSuccess(String response) {
+									Log.v("TeamSelect", "onSuccess");
+									System.out.println(response);
+								}
+
+								
+								@Override
+								public void onFailure(Throwable error, String content) {
+									Log.e("TeamSelect" , "onFailure error : " + error.toString() + "content : " + content);
+									
+									if (content == "Unauthorized") {
+										content = "Please login first.";
+									} 
+									
+									Toast.makeText(getApplicationContext(), "Please authenticate first.", 5).show();
+								}
+
+								
+								@Override
+								public void onFinish() {
+									Log.v("TeamSelect" , "onFinish");
+								}
+							});
+					}
+		        	
+		        });
 			}
         });
 	}
