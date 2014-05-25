25c6fe3c6bcb5a2741f8d4f6f71842180e025498
HKK13 <Kaan_K193@hotmail.com>
2014-05-25 21:24:05 +0300

    Get team info in PracticeCategoriesActivity.

--- Fubio-Android/src/com/st/fubio_android/PractiseCategoriesActivity.java
+++ Fubio-Android/src/com/st/fubio_android/PractiseCategoriesActivity.java
@@ -1,47 +1,49 @@
 package com.st.fubio_android;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+
+import android.os.Bundle;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.st.fubio_android.ServerConnections.RequestManager;
-import com.st.fubio_android.ServerConnections.TinyDB;
-
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-import android.os.Build;
 
 public class PractiseCategoriesActivity extends MainFragment {
 
+	private JSONArray jsonArray;
+	private List<String> nameList, descriptionList, imageList;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_practise_categories);
-
 		RequestManager rm = new RequestManager(getApplicationContext());
+		nameList = new ArrayList<String>();
+		descriptionList = new ArrayList<String>();
+		imageList = new ArrayList<String>();
 		
 		rm.get("practiseCategories", null, new AsyncHttpResponseHandler() {
             	
             	@Override
 				public void onStart() {
             		System.out.println("Practise Categories onStart");
+            		
 				}
 
             	
 				@Override
 				public void onSuccess(String response) {
 					System.out.println(response);
-			
+					try {
+						JSONArray dummyArray = new JSONArray(response);
+						jsonArray = dummyArray;
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+					
 				}
 
 				
@@ -54,8 +56,25 @@ public class PractiseCategoriesActivity extends MainFragment {
 				@Override
 				public void onFinish() {
 					System.out.println("Practise Categories onFinish");
+					try {
+						int position = 0;
+							while(!jsonArray.getJSONObject(position).isNull("name")){
+							nameList.add(jsonArray.getJSONObject(position).getString("name"));
+							descriptionList.add(jsonArray.getJSONObject(position).getString("description"));
+							imageList.add(jsonArray.getJSONObject(position).getString("image"));
+							System.out.println(descriptionList.get(position));
+							position++;
+							}
+						
+					} catch (JSONException e) {
+						
+						e.printStackTrace();
+					}
 				}
             });
+		
+		
+		
 	}
 
 }
