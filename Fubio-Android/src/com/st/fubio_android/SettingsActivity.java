d5dc75fc141f96d6c8a0df027949b21515a2f402
HKK13 <Kaan_K193@hotmail.com>
2014-06-02 02:15:09 +0300

    Update Settings layout.

--- Fubio-Android/src/com/st/fubio_android/SettingsActivity.java
+++ Fubio-Android/src/com/st/fubio_android/SettingsActivity.java
@@ -6,15 +6,14 @@ import android.view.View;
 
 public class SettingsActivity extends MainFragment {
 
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_settings);
-
-		
 	}
-	
+
+
 	public void chooseTeam(View v) {
 		startActivity(new Intent(this, ChooseTeamActivity.class).addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT));
 	}
